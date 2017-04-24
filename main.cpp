33c64812d6154b056b90358f5e09194502481c5d
Jan De Laet <jan.delaet@hotmail.com>
2017-04-24 11:04:21 +0200

    Console gets hidden when starting in release mode

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
  * Makes a factory and starts a game
  */
 
+#include<windows.h>
 #include "Game.h"
 #include "SDLFactory.h"
 
 int main(int argc, char** argv)
 {
+    ShowWindow(GetConsoleWindow(), 0);
+
     NSGame::AbstractFactory* F = new NSSDL::SDLFactory();
     NSGame::Game *game = new NSGame::Game(F);
     game->start();
